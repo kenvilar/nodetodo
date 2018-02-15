598fe2c33bc1197f5023873684340109eb5119e7
Ken Vilar <kenvilar@gmail.com>
2018-02-16 07:24:11 +0800

    Use stream to improve performance

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-	var message = 'RESPONDING...';
-	html = html.replace('{Response}', message);
-	res.end(html);
+	fs.createReadStream(__dirname + '/index.html').pipe(res);
 }).listen(1337, '127.0.0.1');
