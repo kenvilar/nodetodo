83cb163aa8c75bba3fef0cce249c8d0023dbf536
Ken Vilar <kenvilar@gmail.com>
2018-02-16 06:48:00 +0800

    Render the html

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Response.....');
+	var html = fs.readFileSync(__dirname + '/index.html');
+	res.end(html);
 }).listen(1337, '127.0.0.1');
