d2600adba037eba8c932740d9c8e16dfaa5ad64d
Ken Vilar <kenvilar@gmail.com>
2018-02-16 06:43:56 +0800

    Change mime type

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end('Response.....');
 }).listen(1337, '127.0.0.1');
