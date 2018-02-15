3be8fcfd383ce1682aaa389c99e1bfb9df3f5c8e
Ken Vilar <kenvilar@gmail.com>
2018-02-16 07:07:05 +0800

    Set character encoding to manipulate readFileSyn with a string

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	var html = fs.readFileSync(__dirname + '/index.html');
+	var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+	var message = 'RESPONDING...';
+	html = html.replace('{Response}', message);
 	res.end(html);
 }).listen(1337, '127.0.0.1');
