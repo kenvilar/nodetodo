71959f33dc8edecb7b47195fa22e4cd739914e6e
Ken Vilar <kenvilar@gmail.com>
2018-02-16 16:44:37 +0800

    Outputting JSON

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	fs.createReadStream(__dirname + '/index.html').pipe(res);
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	var obj = {
+		firstname: 'Ken',
+		lastname: 'Vilar'
+	};
+	res.end(JSON.stringify(obj));
 }).listen(1337, '127.0.0.1');
