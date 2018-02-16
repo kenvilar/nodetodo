b92fed79c2ccff41fb7c953afd7720065e24a311
Ken Vilar <kenvilar@gmail.com>
2018-02-16 16:53:32 +0800

    Routing of html template, json and 404

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'application/json'});
-	var obj = {
-		firstname: 'Ken',
-		lastname: 'Vilar'
-	};
-	res.end(JSON.stringify(obj));
+	if (req.url === '/') {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + '/index.html').pipe(res);
+	} else if (req.url === '/api') {
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		var obj = {
+			firstname: 'Ken',
+			lastname: 'Vilar'
+		};
+		res.end(JSON.stringify(obj));
+	} else {
+		res.writeHead(404);
+		res.end();
+	}
 }).listen(1337, '127.0.0.1');
