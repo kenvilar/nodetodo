f12997d9a11879af500b054aafea188a79f53722
Ken Vilar <kenvilar@gmail.com>
2018-02-16 18:17:13 +0800

    Route the person/(info)

--- index.js
+++ index.js
@@ -16,4 +16,8 @@ app.get('/api', function (req, res) {
 	res.send(obj);
 });
 
+app.get('/person/:id', function (req, res) {
+	res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
+
 app.listen(port);
