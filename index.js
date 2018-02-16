305485c99e8ba9e0f805c6b2b3e3f13016362d93
Ken Vilar <kenvilar@gmail.com>
2018-02-16 18:41:48 +0800

    Handle middleware

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ var app = express();
 //env port
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function (req, res, next) {
+	console.log('Request URL: ' + req.url);
+	next(); //after using this middleware then proceed to the next middlware
+});
+
 app.get('/', function (req, res) {
-	res.send('<html><head></head><body><h1>RESPONDING...</h1></body></html>');
+	res.send('<html><head><link rel="stylesheet" type="text/css" href="assets/style.css"></head><body><h1>RESPONDING...</h1></body></html>');
 });
 
 app.get('/api', function (req, res) {
