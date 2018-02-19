51484cfc98301c176ce9ea5db6fc04d19a631f5e
Ken Vilar <kenvilar@gmail.com>
2018-02-19 12:55:19 +0800

    Create controller and organize

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var apiController = require('./controllers/apiController');
+var htmlController = require('./controllers/htmlController');
 
 //env port
 var port = process.env.PORT || 3000;
-
-var urlencodedParser = bodyParser.urlencoded({extended: false});
-var jsonParser = bodyParser.json();
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -17,10 +15,6 @@ app.use('/', function (req, res, next) {
 	next(); //after using this middleware then proceed to the next middlware
 });
 
-app.get('/', function (req, res) {
-	res.render('index');
-});
-
 app.get('/api', function (req, res) {
 	var obj = {
 		firstname: 'Ken',
@@ -29,34 +23,7 @@ app.get('/api', function (req, res) {
 	res.send(obj);
 });
 
-app.get('/api/person/:id', urlencodedParser, function (req, res) {
-	//get data
-});
-
-app.post('/api/person', urlencodedParser, function (req, res) {
-	//save
-});
-
-app.delete('/api/person/:id', function (req, res) {
-	//delete
-});
-
-app.get('/person/:id', function (req, res) {
-	res.render('person', {
-		ID: req.params.id
-	});
-});
-
-app.post('/person', urlencodedParser, function (req, res) {
-	res.send('Thank you!');
-	console.log(req.body.firstname);
-	console.log(req.body.lastname);
-});
-
-app.post('/person-json', jsonParser, function (req, res) {
-	res.send('Thank you for the JSON data!');
-	console.log(req.body.firstname);
-	console.log(req.body.lastname);
-});
+htmlController(app);
+apiController(app);
 
 app.listen(port);
