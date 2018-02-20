9cda821fc3225a2da0e9d0487a387b86d1e57772
Ken Vilar <kenvilar@Deestur.local>
2018-02-20 17:48:34 +0800

    Connect db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config/config');
 
 var port = process.env.PORT || 3000;
 
@@ -7,5 +9,6 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-app.listen(port);
+mongoose.connect(config.getDbConnectionString());
 
+app.listen(port);
